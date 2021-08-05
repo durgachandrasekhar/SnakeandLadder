bf61d95010cab8bb4d56870ea8c0b2d7f8a64c84
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-05 18:14:00 +0530

    feat:winning position

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -10,31 +10,34 @@ namespace SnakeandLadder
             const int Snake = 2;
             int position = 0;
             Console.WriteLine("Snake And Ladder problem");
-            Random rand = new Random();
-            int dicenumber = rand.Next(1, 7);
-            Console.WriteLine("The value of dice is " + dicenumber);
-            int options = rand.Next(0, 3);
-
-            switch (options)
+            while (position < 100)
             {
-                case Ladder:
-                    Console.WriteLine("player moves to ladder");
-                    position += dicenumber;
-                    break;
-                case Snake:
-                    Console.WriteLine("player moves to Snake");
-                    position -= dicenumber;
-                    if (position <= 0)
-                    position = 0;
-                    break;
-                default:
-                    Console.WriteLine("if player no play stays in same position");
-                    break;
-                
+                Random rand = new Random();
+                int dicenumber = rand.Next(1, 7);
+                Console.WriteLine("The value of dice is " + dicenumber);
+                int options = rand.Next(0, 3);
+
+                switch (options)
+                {
+                    case Ladder:
+                        Console.WriteLine("player moves to ladder");
+                        position += dicenumber;
+                        break;
+                    case Snake:
+                        Console.WriteLine("player moves to Snake");
+                        position -= dicenumber;
+                        if (position <= 0)
+                            position = 0;
+                        break;
+                    default:
+                        Console.WriteLine("if player no play stays in same position");
+                        break;
+
+                }
+                Console.WriteLine("The Position of Player Wins " + position);
             }
 
 
-            
         }
     }
 }
