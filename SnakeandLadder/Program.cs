3ae81100849140122dd0fb9cffb442c69b835451
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-05 17:15:42 +0530

    feat:single player start at 0

---
+++ SnakeandLadder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeandLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Snake And Ladder problem");
+            int singleplayer = 1;
+            int position = 0;
+        }
+    }
+}
